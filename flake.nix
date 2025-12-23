3e6ed473c3ea13c11eb58782cd65ca99b50b7301
Denys Pavlov <github@denys.me>
2025-12-23 14:20:25 -0500

    chore: init nix flake

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "changeme";
+
+  inputs = {
+    # see docs at https://flake.parts/
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      flake = { };
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          legacyPackages = pkgs;
+          devShells.default = pkgs.mkShell {
+            name = "fc660c-hasu";
+            buildInputs = with pkgs; [
+              qmk
+              dos2unix
+            ];
+          };
+        };
+    };
+}
